684df0f6c4e8a6a912a8d0ccbea061a4a992f2e6
JekRosal <34346272+JekRosal@users.noreply.github.com>
2018-05-11 22:29:43 +0800

    Added New Content to Java Servlets
    Added Front-end design to Sign In Page

---
+++ 9364A-G4/Client/website/Servlet_notes.php
@@ -0,0 +1,7 @@
+<?php
+    include 'subwebsites/Includes/require_session.php';
+
+    include 'subwebsites/Includes/header.php';
+    include 'subwebsites/Includes/sidenav.php';
+    include 'subwebsites/JavaServlet/Servlet_notes.html';
+    include 'subwebsites/Includes/footer.php';
