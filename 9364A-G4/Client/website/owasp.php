9fdb9b8f252fa29a859a36573360a58c22501c1b
JekRosal <34346272+JekRosal@users.noreply.github.com>
2018-05-12 00:05:32 +0800

    Added OWASP image and php
    This is for incoming commit which contains the html file for Owasp

---
+++ 9364A-G4/Client/website/owasp.php
@@ -0,0 +1,7 @@
+<?php
+    include 'subwebsites/Includes/require_session.php';
+
+    include 'subwebsites/Includes/header.php';
+    include 'subwebsites/Includes/sidenav.php';
+    include 'subwebsites/Owasp/Owasp.html';
+    include 'subwebsites/Includes/footer.php';
