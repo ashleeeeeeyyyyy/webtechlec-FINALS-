671bf0606eb1a08a49fd5a1c9abc3c3db616ffb3
JekRosal <34346272+JekRosal@users.noreply.github.com>
2018-05-21 01:46:28 +0800

    Removed excess front-end design from Servlets
    Added additional notes and a pagination in Servlets

---
+++ 9364A-G4/website/Servlet_notes2.php
@@ -0,0 +1,8 @@
+<?php
+    include 'subwebsites/Includes/require_session.php';
+
+    include 'subwebsites/Includes/header.php';
+    include 'subwebsites/Includes/sidenav.php';
+    include 'subwebsites/Includes/nav.php';
+    include 'subwebsites/JavaServlet/Servlet_notes2.html';
+    include 'subwebsites/Includes/footer.php';
